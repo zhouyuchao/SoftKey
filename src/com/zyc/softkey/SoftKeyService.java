590bab4555b0c45bc39de833b3fe49c4519004c4
zhouyuchao <zhouyuchao@coship.com>
2014-07-07 20:21:20 +0800

    20140707 save CMPN

--- src/com/zyc/softkey/SoftKeyService.java
+++ src/com/zyc/softkey/SoftKeyService.java
@@ -8,8 +8,11 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.view.View.OnTouchListener;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
@@ -29,12 +32,13 @@ public class SoftKeyService extends Service {
 
     private Button mSoftKey;
 
-    private float mTouchStartX;
-    private float mTouchStartY;
-    private float x;
-    private float y;
+    private float mPosToViewX;
+    private float mPosToViewY;
+    private float mPosToScreenX;
+    private float mPosToScreenY;
+    private int[] mLocOfView;
 
-    private int statusBarHeight;
+    private int mStatusBarHeight;
     
     @Override
     public IBinder onBind(Intent intent) {
@@ -60,8 +64,8 @@ public class SoftKeyService extends Service {
     public void onCreate() {
         Log.d(TAG, "onCreate --->");
         
-        statusBarHeight = getStatusBarHeight();
-        Log.d(TAG, "onCreate ---> statusBarHeight = " + statusBarHeight);
+        mStatusBarHeight = getStatusBarHeight();
+        Log.d(TAG, "onCreate ---> statusBarHeight = " + mStatusBarHeight);
         
         mWindowManager = (WindowManager) getApplication().getSystemService(WINDOW_SERVICE);
         
@@ -71,7 +75,7 @@ public class SoftKeyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.d(TAG, "onStartCommand --->");
+        Log.d(TAG, "onStartCommand ---> ");
         return super.onStartCommand(intent, flags, startId);
     }
 
@@ -86,12 +90,14 @@ public class SoftKeyService extends Service {
 
     private void showSoftKey() {
         Log.d(TAG, "showSoftKey --->");
+        mLocOfView = new int[2];
+        
         mWindowManager = ((WindowManager) getSystemService("window"));
 
         mWmParams = new WindowManager.LayoutParams();
         mWmParams.width = 90; // LayoutParams.WRAP_CONTENT
         mWmParams.height = 90;
-        mWmParams.gravity = 51;
+        mWmParams.gravity = Gravity.RIGHT | Gravity.CENTER_VERTICAL;
         mWmParams.type = LayoutParams.TYPE_PHONE;
         mWmParams.flags |= LayoutParams.FLAG_NOT_FOCUSABLE;
 
@@ -101,61 +107,81 @@ public class SoftKeyService extends Service {
 
         mSoftKey = new Button(this);
         mSoftKey.setBackgroundResource(R.drawable.ic_floater_hl);
-
+        mSoftKey.setOnTouchListener(skTouchListener);
+        
         mWindowManager.addView(mSoftKey, mWmParams);
-
-        mSoftKey.setOnTouchListener(new OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    Log.d(TAG, "---> ACTION_DOWN <---");
-                    mTouchStartX = event.getX();
-                    mTouchStartY = event.getY();
-//                    Log.d(TAG, "mTouchStartX = " + mTouchStartX + ", mTouchStartY = " + mTouchStartY);
-                    
-                    mSoftKey.setBackgroundResource(R.drawable.ic_floater_hl);
-                    if (mHandler.hasMessages(MSG_UPDATE_SOFTKEY)) {
-                        mHandler.removeMessages(MSG_UPDATE_SOFTKEY);
-                    }
-                    break;
-                case MotionEvent.ACTION_MOVE:
-//                    Log.d(TAG, "---> ACTION_MOVE <---");
-                    x = event.getRawX();
-                    y = event.getRawY() - mSoftKey.getHeight() / 2;
-//                    Log.d(TAG, "x = " + x + ", y = " + y);
-                    
-                    updateViewPosition();
-                    break;
-                case MotionEvent.ACTION_UP:
-                    Log.d(TAG, "---> ACTION_UP <---");
-                    updateViewPosition();
-                    mHandler.sendEmptyMessageDelayed(MSG_UPDATE_SOFTKEY, INT_SOFTKEY_UPDATE_TIME);
-                    break;
-                }
-                return true;
-            }
-        });
-    }
-
-    private void updateViewPosition() {
-        mWmParams.x = ((int) (x - mTouchStartX));
-        mWmParams.y = ((int) (y - mTouchStartY));
-        mWindowManager.updateViewLayout(mSoftKey, mWmParams);
+        
+        mHandler.sendEmptyMessageDelayed(MSG_UPDATE_SOFTKEY, INT_SOFTKEY_UPDATE_TIME);
     }
 
     private int getStatusBarHeight() {
-        if (statusBarHeight == 0) {
+        if (mStatusBarHeight == 0) {
             try {
                 Class c = Class.forName("com.android.internal.R$dimen");
                 Object o = c.newInstance();
                 Field field = c.getField("status_bar_height");
                 int x = (Integer) field.get(o);
-                statusBarHeight = getResources().getDimensionPixelSize(x);
+                mStatusBarHeight = getResources().getDimensionPixelSize(x);
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return statusBarHeight;
+        return mStatusBarHeight;
     }
+    
+    private void updateViewPosition() {
+        mWmParams.x = ((int) (mPosToScreenX - mPosToViewX - mLocOfView[0]));
+        mWmParams.y = ((int) (mPosToScreenY - mPosToViewY - mLocOfView[1]));
+        Log.i(TAG, "updateViewPosition (" + mWmParams.x + ", " + mWmParams.y + ")");
+        mWindowManager.updateViewLayout(mSoftKey, mWmParams);
+    }
+    
+    OnTouchListener skTouchListener = new OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            int statusBarHeight = mStatusBarHeight;
+            /*
+            int[] viewPos = new int[2];
+            mSoftKey.getLocationOnScreen(viewPos);
+            Log.d(TAG, "view position1 (" + viewPos[0] + ", " + viewPos[1] + ")");
+            
+            x = event.getRawX() - viewPos[0];
+            y = event.getRawY() - statusBarHeight - viewPos[1];
+            mWmParams.x = (int) x;
+            mWmParams.y = (int) y;
+            Log.i(TAG, "updateViewPosition (" + mWmParams.x + ", " + mWmParams.y + ")");
+            mWindowManager.updateViewLayout(mSoftKey, mWmParams);
+*/
+            switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                Log.d(TAG, "---> ACTION_DOWN <---");
+                // 获取相对Widget自身左上角的位置
+                mPosToViewX = event.getX();
+                mPosToViewY = event.getY();
+                Log.d(TAG, "mTouchStartX = " + mPosToViewX + ", mTouchStartY = " + mPosToViewY);
+                
+                mSoftKey.setBackgroundResource(R.drawable.ic_floater_hl);
+                
+                if (mHandler.hasMessages(MSG_UPDATE_SOFTKEY)) {
+                    mHandler.removeMessages(MSG_UPDATE_SOFTKEY);
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+//                Log.d(TAG, "---> ACTION_MOVE <---");
+                // 获取相对屏幕左上角的位置
+                mPosToScreenX = event.getRawX();
+                mPosToScreenY = event.getRawY() - statusBarHeight;
+//                Log.d(TAG, "ACTION_MOVE x = " + x + ", y = " + y);
+                
+                updateViewPosition();
+                break;
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "---> ACTION_UP <---");
+                updateViewPosition();
+                mHandler.sendEmptyMessageDelayed(MSG_UPDATE_SOFTKEY, INT_SOFTKEY_UPDATE_TIME);
+                break;
+            }
+            return true;
+        }
+    };
 }
